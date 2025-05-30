b645735acc40c5aa225df28f1e051c2992a61345
Larksel <lemuelgames@outlook.com>
2025-05-30 01:14:14 -0300

    :lipstick: feat: Tela inicial básica

--- PontoAllMobile/app/src/main/java/com/pontoall/pontoallmobile/MainActivity.kt
+++ PontoAllMobile/app/src/main/java/com/pontoall/pontoallmobile/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pontoall.pontoallmobile.ui.theme.PontoAllMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +27,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PontoAllMobileTheme {
-                Surface {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Text(
+                        text = "PontoAll",
+                        color = Color.White,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Blue)
+                            .padding(24.dp)
+                    )
                     MarkPointButton()
                 }
             }
@@ -27,10 +51,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MarkPointButton() {
-    Button(onClick = {
-
-    }) {
-        Text("Marcar Ponto")
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 32.dp),
+        onClick = {}
+    ) {
+        Text(text = "Marcar Ponto", fontSize = 16.sp)
     }
 }
 
@@ -38,6 +65,22 @@ fun MarkPointButton() {
 @Composable
 fun MarkPointButtonPreview() {
     PontoAllMobileTheme {
-        MarkPointButton()
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "PontoAll",
+                color = Color.White,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Blue)
+                    .padding(24.dp)
+            )
+            MarkPointButton()
+        }
     }
 }
