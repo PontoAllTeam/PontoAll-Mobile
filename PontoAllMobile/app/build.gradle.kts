6316632cbda7933dcbd427a03dbf80ed2a9ee460
Larksel <lemuelgames@outlook.com>
2025-05-30 00:06:38 -0300

    :wrench: build: Versão do gradle atualizada

--- PontoAllMobile/app/build.gradle.kts
+++ PontoAllMobile/app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.retrofit2.retrofit)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
