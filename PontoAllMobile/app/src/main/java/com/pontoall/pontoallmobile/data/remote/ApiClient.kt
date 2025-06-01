b061e2c01e29efbc482339b37eb897350bc9118b
Larksel <lemuelgames@outlook.com>
2025-05-31 22:57:59 -0300

    :bug: fix: corrigido versão das dependencias

--- PontoAllMobile/app/src/main/java/com/pontoall/pontoallmobile/data/remote/ApiClient.kt
+++ PontoAllMobile/app/src/main/java/com/pontoall/pontoallmobile/data/remote/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 
 object ApiClient {
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://localhost:7201/api/v1/")
+        .baseUrl("https://10.0.2.2:7201/api/v1/")
         .build()
 
     val markPointService: MarkPointService = retrofit.create(MarkPointService::class.java)
